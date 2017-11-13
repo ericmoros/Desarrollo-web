6bb94e8a30cc2f61f4de87ce6e4f7947ba5fe4cc
Elferic <eric@sunnydale.es>
2017-11-13 11:23:14 +0100

    [Tareas] {Programación} H4_Ej10 //Preparado para desarrollo

---
+++ Programación/Tema - 04/Java/Hoja - 4/Ej - 10/src/H4_Ej10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class H4_Ej10 {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner keyboard = new Scanner(System.in);
+		
+		String inputText;
+		
+		inputText = keyboard.nextLine();
+	}
+}
+
+/*
+Escribir un programa que lea una frase (máximo 60 caracteres) y diga cuántas letras
+distintas tiene y la frecuencia de las mismas, para homogeneizar el conjunto de
+letras debemos trabajar solo con mayúsculas. 
+*/
