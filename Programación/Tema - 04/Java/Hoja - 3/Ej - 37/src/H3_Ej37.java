23ce739436327105dae62bc44107a5334a150796
Elferic - clase <eric@sunnydale.es>
2017-11-16 20:40:37 +0100

    [Clase] {Programación} Ejercicios

--- Programación/Tema - 04/Java/Hoja - 3/Ej - 37/src/H3_Ej37.java
+++ Programación/Tema - 04/Java/Hoja - 3/Ej - 37/src/H3_Ej37.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class H3_Ej37 {
 	public static void main(String[] args) {
 		int rows = 4;
@@ -8,26 +6,47 @@ public class H3_Ej37 {
 		int min = 100;
 		
 		int matriz [][] = new int[rows][columns];
-		
-		int rowKey = 0;
-		for (int[] mRows: matriz) {
-			System.out.println("  ");
-			int cellKey = 0;
-			for (int cells: mRows) {
-				cells = (int)(Math.random() * (max - min + 1) + min);
-				System.out.printf("[%1$4d]", cells);
-				matriz[rowKey][cellKey] = cells;
-				cellKey++;
-			}
-			rowKey++;
-		}
-		
-		System.out.println("\n");
-		for (int[] mRows: matriz) {
-			for (int cells: mRows) {
-				System.out.printf("[%1$4d]", cells);
+
+		for (int rowPos = 0; rowPos < rows; rowPos++) {
+			System.out.println();
+			for (int columnPos = 0; columnPos < columns; columnPos++) {
+				int cells = (int)(Math.random() * (max - min + 1) + min);
+				System.out.printf(" %1$04d ", cells);
+				matriz[rowPos][columnPos] = cells;
 			}
 		}
+		
+		System.out.println();
+		
+		for (int columnPos = 0; columnPos < columns; columnPos++) {
+			System.out.println();
+			for (int rowPos = 0; rowPos < rows; rowPos++) {
+				int cells = matriz[columnPos][rowPos];
+				System.out.printf(" %1$04d ", cells);
+			}
+		}
+		
+//		//___CACA__________________for (fffff=dddd;fff<aaaa;dddd++)____							
+//		int rowKey = 0;
+//		for (int[] mRows: matriz) {
+//			System.out.println("  ");
+//			int cellKey = 0;
+//			for (int cells: mRows) {
+//				cells = (int)(Math.random() * (max - min + 1) + min);
+//				System.out.printf(" %1$04d ", cells);
+//				matriz[rowKey][cellKey] = cells;
+//				cellKey++;
+//			}
+//			rowKey++;
+//		}
+//		//_____________________________________________________________
+//		
+//		System.out.println("\n");
+//		for (int[] mRows: matriz) {
+//			for (int cells: mRows) {
+//				System.out.printf(" %1$04d", cells);
+//			}
+//		}
 		
 	}
 }
