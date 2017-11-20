b2e321f8343f7c1f611bb4b038943d1372fa6d74
Elferic <eric@sunnydale.es>
2017-11-20 02:02:49 +0100

    [Tareas] {Programación} H3_Ej42

--- Programación/Tema - 04/Java/Hoja - 3/Ej - 42/src/H3_Ej42.java
+++ Programación/Tema - 04/Java/Hoja - 3/Ej - 42/src/H3_Ej42.java
@@ -4,24 +4,67 @@ public class H3_Ej42 {
 		final int representatives = 3;
 		final int products = 4;
 		
-		int max = 1;
-		int min = 100;
+		final int max = 1;
+		final int min = 10;
+		
+		int ventas [][][] = new int[representatives][months][products];
+		int totalVentas [] = new int[products];
 		
 		
-		int ventas [][][] = new int[months][representatives][products];
-		
-		System.out.println("Months\n");
-		for (int monthsPos = 0; monthsPos < months; monthsPos++) {
-			System.out.println("Representatives\n");
-			for (int representativesPos = 0; representativesPos < representatives; representativesPos++) {
-				System.out.println("Products\n");
+		for (int representativesPos = 0; representativesPos < representatives; representativesPos++) {
+			System.out.println("\n\n\nRepresentante " + (representativesPos + 1));
+			System.out.print("     ");
+			for (int productsPos = 0; productsPos < products; productsPos++) {
+				System.out.printf("  P%1$-3d", (productsPos + 1));
+			}
+			for (int monthsPos = 0; monthsPos < months; monthsPos++) {
+				System.out.printf("\n M%1$-3d", (monthsPos + 1));
 				for (int productsPos = 0; productsPos < products; productsPos++) {
 					int cells = (int)(Math.random() * (max - min + 1) + min);
-					cells = ventas[monthsPos][representativesPos][productsPos];
-					System.out.printf(" %1$04d ", cells);
+					System.out.printf("  %1$04d", cells);
+					ventas[representativesPos][monthsPos][productsPos] = cells;
 				}
 			}
+			
+			System.out.print("\n     ");
+			for (int productsPos = 0; productsPos < products; productsPos++) {
+				System.out.print("  ----");
+			}
+			System.out.print("\nTotal");
+			for (int productsPos = 0; productsPos < products; productsPos++) {
+				int productsTotal = 0;
+				for (int monthsPos = 0; monthsPos < months; monthsPos++) {
+					int cells = ventas[representativesPos][monthsPos][productsPos];
+					productsTotal += cells;
+				}
+				System.out.printf("  %1$04d", productsTotal);
+				totalVentas[productsPos] += productsTotal;
+			}
 		}
+		
+		
+		System.out.print("\n\n\n +-----");
+		for (int productsPos = 0; productsPos < products; productsPos++) {
+			System.out.print("+-----");
+		}
+		System.out.print("+\n |Re/Pr");
+		for (int productsPos = 0; productsPos < products; productsPos++) {
+			System.out.printf("|P%1$-4d", (productsPos + 1));
+		}
+		System.out.print("|\n +-----");
+		for (int productsPos = 0; productsPos < products; productsPos++) {
+			System.out.print("+-----");
+		}
+		
+		System.out.print("+\n |Total");
+		for (int representativesVentas: totalVentas) {
+			System.out.printf("|%1$05d", representativesVentas);
+		}
+		System.out.print("|\n +-----");
+		for (int productsPos = 0; productsPos < products; productsPos++) {
+			System.out.print("+-----");
+		}
+		System.out.print("+");
 	}
 }
 
