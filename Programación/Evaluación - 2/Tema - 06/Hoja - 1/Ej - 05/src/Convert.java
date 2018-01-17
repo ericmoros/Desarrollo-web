e57305c6633f2f5b4d8d76b5d8ec04a6b0fed28e
Elferic - clase <eric@sunnydale.es>
2018-01-17 21:19:04 +0100

    [Tareas] {Programaicón}  Ej - 05 //Depurando lector de fechas

--- Programación/Evaluación - 2/Tema - 06/Hoja - 1/Ej - 05/src/Convert.java
+++ Programación/Evaluación - 2/Tema - 06/Hoja - 1/Ej - 05/src/Convert.java
@@ -1,8 +1,24 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Convert {
-	private static HashMap<String, Integer> monthMap =  new HashMap<String, Integer>(){///No me gusta
+//	private static ArrayList<Integer> asciiNumList = new ArrayList<Integer>() {//Repasar
+//		{
+//			add(48);
+//			add(49);
+//			add(50);
+//			add(51);
+//			add(52);
+//			add(53);
+//			add(54);
+//			add(55);
+//			add(56);
+//			add(57);
+//		}
+//	};
+	
+	private static HashMap<String, Integer> monthMap = new HashMap<String, Integer>() {
 		{
 			put("Enero", 1);
 			put("Febrero", 2);
@@ -16,40 +32,45 @@ public class Convert {
 			put("Octubre", 10);
 			put("Noviembre", 11);
 			put("Diciembre", 12);
-	    }
+		}
 	};
-	
-	private static ArrayList<String> monthList = new ArrayList<String>(){
+
+	private static ArrayList<String> monthList = new ArrayList<String>() {
 		{
-			ArrayList<String> monthList = new ArrayList<>();
-			
-			for (String month: monthMap.keySet()) {
-				monthList.add(month);
+			for (String month : monthMap.keySet()) {
+				add(month);
 			}
 		}
 	};
-	
-	public static Integer stringNumber (String str) {
+
+	public static Integer stringNumber(String str) {
 		Integer number = Integer.parseInt(str);
 		return number;
 	}
 	
-	private static Date textDatetoDate (String day, String month, String year) {
+	public static String numberString(Integer num) {
+		String number = "" + num;
+		return number;
+	}
+
+	private static Date textDateToDate(String day, String month, String year) {
 		Date date;
 		Integer nDay;
 		Integer nMonth;
 		Integer nYear;
-		
-		nDay   = stringNumber(day);
+
+		nDay = stringNumber(day);
 		nMonth = monthMap.get(month);
-		nYear  = stringNumber(year);
-		
-		date = new Date (nDay, nMonth, nYear);
-		
-		return date;
+		nYear = stringNumber(year);
+
+		date = new Date(nDay, nMonth, nYear);
+
+		return date;//here
 	}
-	
-	public static Date textDate (String text) {
+
+	public static Date textDate(String text) {
+		String scanText = text;
+		
 		Date date;
 		String day;
 		String month;
@@ -57,52 +78,27 @@ public class Convert {
 		
 		
 		
-		return null; //HERE!!!!!
+		day = Extract.strNumberRangeFromText(text, 1, 31);
+		scanText = day//////HERE!!!!!!
+		month = Extract.wordFromText(text, monthList);
+		
+		
+		return null;
 	}
-	
-	public static Integer dateToNumber (Date date) {
-		//Date
-		Integer day		= date.getDay();
-		Integer month	= date.getMonth();
-		Integer year	= date.getYear();
-		
-		//Num
+
+	public static Integer dateToNumber(Date date) {
+		// Date
+		Integer day = date.getDay();
+		Integer month = date.getMonth();
+		Integer year = date.getYear();
+
+		// Num
 		Integer number = 0;
-		
+
 		for (Integer yearPos = 0; yearPos < year; yearPos++) {
 			for (Integer monthPos = 1; monthPos <= 12; monthPos++) {
 				switch (monthPos) {
-					//31
-					case 1:
-					case 3:
-					case 5:
-					case 7:
-					case 8:
-					case 10:
-					case 12:
-						number += 31;
-						break;
-					//30
-					case 4:
-					case 6:
-					case 9:
-					case 11:
-						number += 30;
-						break;
-					//29 o 28
-					case 2:
-						if (year % 4 == 0 ) { ////////////////
-							number += 29;
-						} else {
-							number += 28;
-						}
-				}
-			}
-		}
-		
-		for (Integer monthPos = 1; monthPos < month; monthPos++) {
-			switch (monthPos) {
-				//31
+				// 31
 				case 1:
 				case 3:
 				case 5:
@@ -112,27 +108,57 @@ public class Convert {
 				case 12:
 					number += 31;
 					break;
-				//30
+				// 30
 				case 4:
 				case 6:
 				case 9:
 				case 11:
 					number += 30;
 					break;
-				//29 o 28
+				// 29 o 28
 				case 2:
-					if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) { ////////////////
+					if (year % 4 == 0) { ////////////////
 						number += 29;
 					} else {
 						number += 28;
 					}
+				}
 			}
 		}
-		
+
+		for (Integer monthPos = 1; monthPos < month; monthPos++) {
+			switch (monthPos) {
+			// 31
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				number += 31;
+				break;
+			// 30
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				number += 30;
+				break;
+			// 29 o 28
+			case 2:
+				if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) { ////////////////
+					number += 29;
+				} else {
+					number += 28;
+				}
+			}
+		}
+
 		for (Integer dayPos = 0; dayPos < day; dayPos++) {
 			number++;
 		}
-		
+
 		return number;
 	}
 }
--- Programación/Evaluación - 2/Tema - 06/Hoja - 1/Ej - 05/src/Extract.java
+++ Programación/Evaluación - 2/Tema - 06/Hoja - 1/Ej - 05/src/Extract.java
@@ -1,20 +1,56 @@
 import java.util.ArrayList;
 
 public class Extract {
-	public static String wordFromText (String text, String word) {
+	@SuppressWarnings("null")
+	public static ArrayList<Integer> numberRange(Integer from, Integer to) {
+		ArrayList<Integer> nums = null;
+		
+		for (Integer posNum = from; posNum <= to; posNum++) {
+			nums.add(posNum);
+		}
+		
+		return nums;
+	}
+	
+	public static String wordFromText(String text, String word) {
 		if (text.contains(word)) {
 			return word;
 		} else {
 			return null;
 		}
 	}
-	public static ArrayList<String> wordsFromText (String text, ArrayList<String> words) {
-		ArrayList<String> wordsFound = new ArrayList<>();
+
+	public static String wordFromText(String text, ArrayList<String> words) {
+		String wordFound = null;
+
+		for (String word : words) {
+			if (text.contains(word)) {
+				wordFound = word;
+				break;
+			}
+		}
+
+		return wordFound;
+	}
+	
+	public static String strNumberRangeFromText(String text, Integer from, Integer to) {//////HERE!!!!!!
+		String numFound = null;
 		
-		for (String word: words) {
-			words.add(wordFromText(text, word));
+		for (Integer num: numberRange(from, to)) {
+			numFound = Convert.numberString(num);
+			break;
 		}
 		
+		return numFound;
+	}
+
+	public static ArrayList<String> wordsFromText(String text, ArrayList<String> words) {
+		ArrayList<String> wordsFound = new ArrayList<>();
+
+		for (String word : words) {
+			words.add(wordFromText(text, word));
+		}
+
 		return wordsFound;
 	}
 }
