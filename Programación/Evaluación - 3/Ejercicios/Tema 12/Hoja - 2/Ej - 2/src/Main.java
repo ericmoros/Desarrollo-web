f30752dad6fa10e0e8f59f629a30d9462dda2774
Elferic <eric@sunnydale.es>
2018-04-19 00:45:27 +0200

    [Clase] {Programación} Ej - 2 //Finalizado
    Co-authored-by: Jose Antonio <tellajoseantonioxd@gmail.com>

--- Programación/Evaluación - 3/Ejercicios/Tema 12/Hoja - 2/Ej - 2/src/Main.java
+++ Programación/Evaluación - 3/Ejercicios/Tema 12/Hoja - 2/Ej - 2/src/Main.java
@@ -2,54 +2,11 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.TreeMap;
 
-/**
- * 
- */
-
-/**
- * @author Eric Moros Pérez
- *
- */
 public class Main {
-
-	/**
-	 * @param args
-	 * 
-	 *            Este ejercicio pretende desarrollar el programa de cálculo de
-	 *            escaños en unas elecciones, teniendo como datos de entrada: El
-	 *            número de partidos que se presentan (N), el número de escaños a
-	 *            repartir (M) y los votos que ha obtenido cada partido A partir de
-	 *            aquí, hay que hacer lo siguiente se dividen los votos de cada
-	 *            partido entre 1, 2, 3....M y se obtienen M divisiones y así
-	 *            sucesivamente para el partido 2,3,...N es decir se obtendrán NxM
-	 *            cocientes, y de esos cocientes se eligen los M mayores, puesto que
-	 *            hay M diputados a repartir Un ejemplo sería el siguiente:
-	 * 
-	 *            PARTIDOS: 4 ESCAÑOS: 5 Escaños Partidos 1 2 3 4 5 Part1 10000 5000
-	 *            3333 2500 2000 Part2 25000 12500 8333 6250 5000 Part3 30000 15000
-	 *            10000 7500 6000 Part4 12000 6000 4000 3000 2400
-	 * 
-	 *            Una vez hechos los cálculos habrá que ver a qué partidos le
-	 *            corresponden los 5 mayores cocientes. El listado de resultados
-	 *            debe hacerse ordenado por número de votos. Los partidos que no
-	 *            obtengan un 5% de los votos emitidos deben rechazarse, y no
-	 *            entrarán en el reparto de escaños. NOTA: Obligatorio el uso de
-	 *            colecciones para resolverlo, se puede usar un TreeMap con clave el
-	 *            cociente y valor asociado un TreeSet que en cada elemento guarda
-	 *            el nombre del partido a quien pertenece ese cociente. El mapa debe
-	 *            estar ordenado por los valores de los cocientes de mayor a menor.
-	 *            Si hay M escaños los M primeros elementos de los TreeSet que se
-	 *            guardan en el mapa nos indican a quien corresponden los escaños.
-	 *            En este ejercicio nos interesa recorrer los primeros nodos del
-	 *            mapa y llevar la cuenta del número de elementos recorridos de los
-	 *            TreeSet que van asociados a cada nodo porque cuando tengamos 5 en
-	 *            total se termina. Se pueden utilizar otros enfoques con
-	 *            colecciones para resolverlo.
-	 */
 	public static void main(String[] args) {
 		Integer totalEscanyos;
 		Integer totalPartidos;
-		Integer porcentajeMin;
+		Integer porcentajeMin, totalVotos;
 
 		TreeMap<Integer, LinkedList<Partido>> treeVotos = new TreeMap<>(Collections.reverseOrder());
 
@@ -59,21 +16,22 @@ public class Main {
 
 		Partido vPartidos[] = new Partido[totalPartidos];
 
-		calcularVotos(treeVotos, vPartidos, totalEscanyos, totalPartidos, porcentajeMin);
+		totalVotos = calcularVotos(treeVotos, vPartidos, totalEscanyos, totalPartidos, porcentajeMin);
 		calcularEscanyos(treeVotos, totalEscanyos);
-		podar(treeVotos);
+		Leer.mostrarEnPantalla("Arbol optimizado: " + podar(treeVotos) + "%");
 
-		mostrarPartidosVotos(vPartidos);
+		mostrarPartidosVotos(vPartidos, totalVotos);
 		mostrarEscanyos(vPartidos);
-
+		
 	}
 
-	private static void calcularVotos(TreeMap<Integer, LinkedList<Partido>> treeVotos, Partido[] vPartidos,
-			Integer totalEscanyos, Integer totalPartidos, Integer porcentajeMin) {
+	private static Integer calcularVotos(TreeMap<Integer, LinkedList<Partido>> treeVotos,
+		Partido[] vPartidos, Integer totalEscanyos, Integer totalPartidos, Integer porcentajeMin) {
 		Integer totalVotos = 0;
 		Integer votos;
 		Partido pPartido = null;
 		Integer totalVotosMin = null;
+		
 
 		for (Integer i = 0; i < totalPartidos; i++) {
 			votos = (int) Math.floor(Math.random() * (30000 - 50 + 1) + (50));
@@ -88,16 +46,17 @@ public class Main {
 				treeVotos.get((votos / j)).add(pPartido);
 			}
 		}
-
+		
 		totalVotosMin = totalVotos * porcentajeMin / 100;
-
-		for (Partido partido : vPartidos) {
+		
+		for (Partido partido: vPartidos) {
 			if (partido.getVotos() <= totalVotosMin) {
 				for (Integer key : treeVotos.keySet()) {
 					treeVotos.get(key).remove(partido);
 				}
 			}
 		}
+		return totalVotos;
 	}
 
 	private static void calcularEscanyos(TreeMap<Integer, LinkedList<Partido>> treeVotos, Integer totalEscanyos) {
@@ -105,27 +64,27 @@ public class Main {
 
 		for (Integer votos : treeVotos.keySet()) {
 			if (contaEscanyos < totalEscanyos) {
-				for (Partido partidos : treeVotos.get(votos)) {
-					contaEscanyos++;
-					partidos.addEscanyos();
-				}
-			} else {
+					for (Partido partidos: treeVotos.get(votos)) {
+						contaEscanyos++;
+						partidos.addEscanyos();
+					}
+			}else {
 				break;
 			}
-
 		}
 	}
 
-	private static void mostrarPartidosVotos(Partido[] vPartidos) {
-		String partidosMostrar = "\nPARTIDOS\tVOTOS";
+	private static void mostrarPartidosVotos(Partido[] vPartidos, Integer totalVotos) {
+		String partidosMostrar = "\nPARTIDOS\tVOTOS\t"+totalVotos;
 		String pNombre;
 		Integer pVotos;
+		Float porcentaje;
 		Leer.mostrarEnPantalla(partidosMostrar);
 		for (Partido partido : vPartidos) {
 			pNombre = partido.getNombre();
 			pVotos = partido.getVotos();
-			// pEscanyos = par
-			Leer.mostrarEnPantalla(pNombre + "\t" + pVotos);
+			porcentaje = (float)(100 * pVotos) / (float) totalVotos;
+			Leer.mostrarEnPantalla(pNombre + "\t" + pVotos + "\t" + porcentaje + "%");
 		}
 	}
 
@@ -133,6 +92,7 @@ public class Main {
 		String pNombre;
 		String escanyosMostrar = "\nPARTIDOS\tESCAÑOS";
 		Integer pEscanyos;
+		Partido winner = vPartidos[0];
 		Leer.mostrarEnPantalla(escanyosMostrar);
 		for (Partido partido : vPartidos) {
 			pNombre = partido.getNombre();
@@ -140,18 +100,32 @@ public class Main {
 
 			Leer.mostrarEnPantalla(pNombre + "\t" + pEscanyos);
 		}
-	}
-
-	private static Integer podar(TreeMap<Integer, LinkedList<Partido>> treeVotos) {
-		for (Integer key: new TreeMap<Integer, LinkedList<Partido>>(treeVotos).keySet()) {
-			if (treeVotos.get(key).size() == 0) {
-				treeVotos.remove(key);
-				System.out.println("Arbol optimizado!");
-
+		
+		for (Partido i: vPartidos) {
+			if (winner.getEscanyos() < i.getEscanyos()) {
+				winner = i;
 			}
 		}
-
-		return null;
+		
+		Leer.mostrarEnPantalla("\nPartido con más escaños: " + winner.getNombre());
+		
 	}
-
+	
+	private static Float podar(TreeMap<Integer, LinkedList<Partido>> treeVotos) {
+		Float optimizacion;
+		Integer totalKeys = 0;
+		Integer ramasPodadas = 0;
+		for (Integer key: new TreeMap<Integer, LinkedList<Partido>>(treeVotos).keySet()) {
+			totalKeys++;
+			if (treeVotos.get(key).size() == 0) {
+				treeVotos.remove(key);
+				ramasPodadas++;
+			}
+		}
+		
+		optimizacion = (float) (ramasPodadas * 100) / (float) totalKeys;
+		
+		return optimizacion;
+	}
+	
 }
