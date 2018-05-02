46b3a5013da09261005d21811a4621404c0781c9
Eric Moros Pérez <eric@sunnydale.es>
2018-05-02 21:19:10 +0200

    [Clase] {Programación} Ej - 4 //A medias
    Co-authored-by: Jose Antonio <tellajoseantonioxd@gmail.com>

--- Programación/Evaluación - 3/Ejercicios/Tema 12/Hoja - 2/Ej - 4/src/Main.java
+++ Programación/Evaluación - 3/Ejercicios/Tema 12/Hoja - 2/Ej - 4/src/Main.java
@@ -34,29 +34,106 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		TreeMap<User, String> usrPassList = new TreeMap<>();
-		User user = new User();
-		user = createUser();
-		signup(usrPassList, user);
-		printUsers(usrPassList);
+		String options = "1";
+		Boolean exit = false;
+
+		while (true) {
+			for (Character option : options.toCharArray()) {
+				switch (option) {
+					case '1':
+						Leer.mostrarEnPantalla("_ Menu __________________\n"
+											 + " 1 - Mostrar menu\n"
+											 + " 2 - Registrar usuario/s \n"
+											 + " 2 - Modificar contraseña \n"
+											 + "_________________________\n");
+						break;
+					case '2':
+						signupUsers(usrPassList);
+						break;
+					case '3':
+						modifyUserPassword(usrPassList);
+						break;
+					case '0':
+						exit = true;
+						break;
+					default:
+						options += '1';
+				}
+
+			}
+			
+			if (exit) break;
+
+			options = Leer.pedirCadena("Introduce una de las opciones");
+			
+		}
+	}
+
+	/**
+	 * @param usrPassList 
+	 * 
+	 */
+	private static void modifyUserPassword(TreeMap<User, String> usrPassList) {
+		User user = null;
+		String name;
+		String password;
+		Integer security;
+		String encryptedPass;
+		Leer.mostrarEnPantalla("[Modificar contraseña]");
+		name = Leer.pedirCadena("Nombre del usuario");
+		
+		for (User tmpUser : usrPassList.keySet()) {
+			if (tmpUser.getNickname().equals(name)) {
+				user = tmpUser;
+				break;
+			}
+		}
+		
+		if (user != null) {
+			password = Leer.pedirCadena("Introduce la nueva contraseña");
+			security = Leer.pedirEntero("Nivel de seguridad");
+			encryptedPass = encryptPass(password, security);
+			user.setPassword(encryptedPass);
+			usrPassList.put(user, encryptedPass);
+		} else {
+			Leer.mostrarEnPantalla("El usuario" + name + " no existe");
+		}
 		
 	}
 
-	private static User createUser() {
-		User user = new User();
-		String nickname;
-		String password;
-		String encryptedPass;
-		Integer moveNumber;
+	private static void signupUsers(TreeMap<User, String> usrPassList) {
+		ArrayList<User> users = new ArrayList<>();
+		users = createUsers();
+		signup(usrPassList, users);
+		printUsers(usrPassList);
+	}
 
+	private static ArrayList<User> createUsers() {
+		ArrayList<User> users = new ArrayList<>();
+		String exit = "0";
+		User user = new User();
+		String nickname = "";
+		String password = "";
+		String encryptedPass = "";
+		Integer moveNumber = 0;
 		
-		nickname = Leer.pedirCadena("Nombre de usuario:");
-		password = Leer.pedirCadena("Contraseña:");
-		moveNumber = Leer.pedirEntero("Seguridad de encriptado");
-		//String passwordConfirm;
-		encryptedPass = encryptPass(password, moveNumber);
-		user.setNickname(nickname);
-		user.setPassword(encryptedPass);
-		return user;
+		Leer.mostrarEnPantalla("[Registrar usuario/s]");
+
+		while (true) {
+			Leer.mostrarEnPantalla("(0 para salir)");
+			nickname = Leer.pedirCadena("Nombre de usuario:");
+			if (nickname.equals(exit)) break;
+			password = Leer.pedirCadena("Contraseña:");
+			if (password.equals(exit)) break;
+			moveNumber = Leer.pedirEntero("Seguridad de encriptado");
+			if (moveNumber == Integer.parseInt(exit)) break;
+			// String passwordConfirm;
+			encryptedPass = encryptPass(password, moveNumber);
+			user.setNickname(nickname);
+			user.setPassword(encryptedPass);
+			users.add(user);
+		}
+		return users;
 	}
 
 	private static void printUsers(TreeMap<User, String> usrPassList) {
@@ -70,8 +147,10 @@ public class Main {
 		}
 	}
 
-	private static void signup(TreeMap<User, String> usrPassList, User user) {
-		usrPassList.put(user, user.getPassword());
+	private static void signup(TreeMap<User, String> usrPassList, ArrayList<User> users) {
+		for (User user : users) {		
+			usrPassList.put(user, user.getPassword());
+		}
 	}
 	
 	public static String encryptPass(String password, Integer moveNumber) {
