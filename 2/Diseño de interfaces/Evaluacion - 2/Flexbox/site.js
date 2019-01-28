25eaab8d51482cd4ecba2dc2f7646f7cb720350e
Elferic <eric@sunnydale.es>
2019-01-28 16:22:14 +0100

    {Casa} [DI] Flexbox

---
+++ 2/Diseño de interfaces/Evaluacion - 2/Flexbox/site.js
@@ -0,0 +1,24 @@
+var site = {
+    adds: {
+        current: 0,
+        images: [
+            'img/add-1.png',
+            'img/add-2.png',
+            'img/add-3.png'
+        ]
+    }
+
+}
+
+window.onload = afterTime;
+
+function afterTime() {
+    setInterval(function () {
+        if (site.adds.current < site.adds.images.length - 1) {
+            site.adds.current++;
+        } else {
+            site.adds.current = 0;
+        }
+        add.src = site.adds.images[site.adds.current];
+    }, 10000);
+}
